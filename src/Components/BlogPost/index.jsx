8e0b3468fbeeea90e3e59798d6c03e3a53427701
Donovan Rojas <rojasdrm03@gmail.com>
2024-02-22 14:05:43 -0500

    useNavigate to return to blog

--- src/Components/BlogPost/index.jsx
+++ src/Components/BlogPost/index.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { blogdata } from "../../data/blogdata";
 
 function BlogPost() {
-    const { slug } = useParams();
+  const navigate = useNavigate();
+  const { slug } = useParams();
 
     const blogpost = blogdata.find(post => post.slug === slug);
+    const returnToBlog = () => {
+      navigate('/blog');
+      // navigate(-1);//vuelve al historial anterior
+    };
 
   return (
     <>
       <h2>{blogpost.title}</h2>
       <p>{blogpost.content}</p>
       <p>{blogpost.author}</p>
+      <button onClick={returnToBlog}>Volver al blog</button>
     </>
   );
 }
