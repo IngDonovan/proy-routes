b48a2c8fc84d9e6f6e91f62d9087a234c54b53bf
Donovan Rojas <rojasdrm03@gmail.com>
2024-02-22 09:38:00 -0500

    use NavLink in navbar with dinamic Object

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,9 +1,96 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Menu.css'
 
 function Menu() {
   return (
-    <h1>Menu</h1>
+    <nav>
+      <ul>
+      {routes2.map(route => (
+          <li key={route.id}>
+            <NavLink
+              
+              style={({ isActive }) => ({
+                color: isActive ? 'red' : 'blue',
+              })}
+              to={route.to}
+              end
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        ))}
+        {/* <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li> */}
+
+        {/* <li>
+          <NavLink
+            className={({ isActive }) => ()}
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'blue',
+            })}
+            to="/"
+          >Home</NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'blue',
+            })}
+            to="/blog"
+          >Blog</NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'blue',
+            })}
+            to="/profile"
+          >Profile</NavLink>
+        </li> */}
+
+      </ul>
+    </nav>
   );
 }
+
+const routes = [];
+routes.push({
+  to: '/',
+  text: 'Home',
+});
+routes.push({
+  to: '/blog',
+  text: 'Blog',
+});
+routes.push({
+  to: '/profile',
+  text: 'Profile',
+});
+
+const routes2 = [
+  {
+    id:1,
+    to: '/',
+    text: 'Home',
+  },
+  {
+    id:2,
+    to: '/blog',
+    text: 'Blog',
+  },
+  {
+    id:3,
+    to: '/profile',
+    text: 'Profile',
+  },
+];
 
 export { Menu };
